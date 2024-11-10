1ba389a6eb166615d041c686c07e7ae6439706da
Parth Mangtani <impmmmm@gmail.com>
2024-11-10 08:32:53 +0530

    💚 bug fixes in build

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
@@ -7,7 +5,7 @@ export default function Home() {
     <main className="bg-black h-screen w-screen flex items-center justify-center">
       <div className="h-[70vmin] w-[70vmin] flex flex-col gap-4 items-center  justify-center bg-[#00ffff]">
         <h1 className="text-5xl lg:text-6xl">welcome</h1>
-        <h2 className="text-l lg:text-xl">to the thePmSquare's server.</h2>
+        <h2 className="text-l lg:text-xl">to the thePmSquare&apos;s server.</h2>
         <div>nothing much to do here. :/</div>
       </div>
 
