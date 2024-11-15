5dceacd2c573aadeecf57d302323b09f481fe9da
Parth Mangtani <impmmmm@gmail.com>
2024-11-15 19:27:37 +0530

    🔧 change default url

--- src/config/square.ts
+++ src/config/square.ts
@@ -1,5 +1,5 @@
 const squareConfig = {
-  squareCommonBLBaseURL: "http://localhost:10110",
+  squareCommonBLBaseURL: "https://raspi.thepmsquare.com:10110",
   appName: "raspi_home",
 };
 export default squareConfig;
